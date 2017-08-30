b821647252f900fc6d639c78bc48eed6b22d190d
Ilya <i.dovzhenko26@gmail.com>
2017-08-30 13:00:23 +0700

    ref some parts

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -1,17 +1,9 @@
-import jdk.internal.org.xml.sax.Attributes;
-
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
@@ -19,13 +11,14 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DBConnector {
 
     public static void getConnection(long num) {
         String connectionUrl = "jdbc:sqlite:introDB.db";
         String Driver = "org.sqlite.JDBC";
+        String file1 = "1.xml";
+        String file2 = "2.xml";
 
         try {
             Class.forName(Driver);
@@ -41,11 +34,9 @@ public class DBConnector {
 
             //Пакетное выполнение sql запросов
             connection.setAutoCommit(false);
-            String sqlString;
             StringBuilder builder = new StringBuilder();
 
             for(Integer i = 1; i <= num; i++) {
-                //sqlString = new StringBuilder("INSERT INTO TEST (field) VALUES (" + i + ")").toString();
                 builder.append("INSERT INTO TEST (field) VALUES (" + i + ")");
                 statement.addBatch(builder.toString());
                 builder.delete(0, Integer.MAX_VALUE);
@@ -71,63 +62,56 @@ public class DBConnector {
 
             XMLOutputter xmlOutput = new XMLOutputter();
             xmlOutput.setFormat(Format.getPrettyFormat());
-            xmlOutput.output(doc, new FileWriter("1.xml"));
+            xmlOutput.output(doc, new FileWriter(file1));
             entry.clear();
 
             //Преобразование данных из 1.xml в 2.xml с помощью xslt
-            try {
-                TransformerFactory factory = TransformerFactory.newInstance();
-                Source xsl = new StreamSource(new File("transform.xsl"));
-                Transformer transformer = factory.newTransformer(xsl);
-
-                Source text = new StreamSource("1.xml");
-                transformer.transform(text, new StreamResult("2.xml"));
-            } catch(TransformerConfigurationException e) {
-                e.printStackTrace();
-            } catch(TransformerException e) {
-                e.printStackTrace();
-            }
+            transformXML(file1, file2);
 
             //Парсинг 2.xml для сложения всех данных
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            factory.setValidating(true);
-            factory.setNamespaceAware(false);
-            SAXParser parser;
+            SAXParserFactoryParse(file2);
 
-            InputStream xmlData = null;
-
-            try {
-                xmlData = new FileInputStream("2.xml");
-
-                parser = factory.newSAXParser();
-                parser.parse(xmlData, new MyHandler());
-            } catch(SAXException e) {
-                e.printStackTrace();
-            } catch(ParserConfigurationException e) {
-                e.printStackTrace();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-
-            //Парсинг 2.xml для сложения всех данных
-            /*try {
-                SAXBuilder saxBuilder = new SAXBuilder();
-                Document document = saxBuilder.build("2.xml");
-                Element classElement = document.getRootElement();
-                List<Element> entriesList = classElement.getChildren();
-                long sum = 0;
-                for (int temp = 0; temp < entriesList.size(); temp++) {
-                    Element entries2 = entriesList.get(temp);
-                    Attribute attribute =  entries2.getAttribute("field");
-                    sum = sum + attribute.getIntValue();
-                }
-                System.out.println("Summa = " + sum);
-            } catch(JDOMException e) {
-                e.printStackTrace();
-            }*/
         } catch (SQLException e) {
             e.printStackTrace();
         } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void transformXML(String inFile, String outFile) {
+        //Преобразование данных из 1.xml в 2.xml с помощью xslt
+        try {
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Source xsl = new StreamSource(new File("transform.xsl"));
+            Transformer transformer = factory.newTransformer(xsl);
+
+            Source text = new StreamSource(inFile);
+            transformer.transform(text, new StreamResult(outFile));
+        } catch(TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch(TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void SAXParserFactoryParse(String fileName) {
+        //Парсинг 2.xml для сложения всех данных
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating(true);
+        factory.setNamespaceAware(false);
+        SAXParser parser;
+        InputStream xmlData = null;
+
+        try {
+            xmlData = new FileInputStream(fileName);
+
+            parser = factory.newSAXParser();
+            parser.parse(xmlData, new MyHandler());
+        } catch(SAXException e) {
+            e.printStackTrace();
+        } catch(ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
@@ -143,8 +127,5 @@ public class DBConnector {
             }
             super.startElement(uri, localName, qName, attributes);
         }
-
-
     }
-
 }
